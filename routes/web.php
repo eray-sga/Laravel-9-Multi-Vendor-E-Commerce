9ca4305f6a329da0303962cc051d54acca7b8ba1
eraykalkan <eraykalkan6@gmail.com>
2022-03-27 17:11:02 +0300

    admin dashboard, admin controller, dashboard route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+//Admin Dashboard Route without admin group
+Route::get('admin/dashboard','App\Http\Controllers\Admin\AdminController@dashboard');
